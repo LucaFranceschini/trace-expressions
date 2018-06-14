882fac959741d405fcc5fc38bbb9eba0058d0903
davideancona <davide.ancona@unige.it>
2018-06-14 09:12:23 +0200

    benchmark

---
+++ examples/express-examples/fileExplorer/transpiled/simple_server-ecma5-no-instr.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var morgan = require('morgan');
+var responseTime = require('response-time')
+var serveIndex = require('serve-index')
+
+var path = process.cwd()
+
+var app = express()
+
+app.use(morgan('combined'))
+app.use(responseTime())
+app.use('/', express.static(path), serveIndex(path,{'icons': true}))
+app.get('/*',function(req,res){
+    res.writeHead(404,{'Content-Type':'text/html'})
+    return res.end('Not found\n')
+})
+
+app.listen(8080)
