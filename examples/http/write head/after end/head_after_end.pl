66a173cb473a620e4f513596a9872d9b665823df
davideancona <davide.ancona@unige.it>
2018-09-28 10:59:46 +0200

    benchmark

--- examples/http/write head/after end/head_after_end.pl
+++ examples/http/write head/after end/head_after_end.pl
@@ -1,33 +1,23 @@
-:- module(spec,[trace_expression/2, match/3]).
-
-:- discontiguous match/3.
-:- discontiguous event/2.
+:- module(spec,[trace_expression/2, match/2]).
 
 :- use_module(node(func_match)).
 
-match(head_after_end,json(O), filter) :- func_pre_names(json(O) , ['end' , 'writeHead' , 'http.createServer']).
-event(head_after_end, json(_)).
+match(Json, filter) :- func_pre_names(Json , ['end' , 'writeHead' , 'http.createServer']).
 
-match(head_after_end,json(O), createServer) :- func_pre_name(json(O) , 'http.createServer').
-event(head_after_end, json(_)).
+match(Json, createServer) :- func_pre_name(Json , 'http.createServer').
 
-match(head_after_end,json(O), end(ResponseId)) :- func_pre(json(O) , 'end' , _ , _ , ResponseId).
-event(head_after_end, json(_)).
+match(Json, end(ResponseId)) :- func_pre(Json , 'end' , _ , _ , ResponseId).
 
-match(head_after_end,json(O), writeHead(ResponseId)) :- func_pre(json(O) , 'writeHead' , _ , _ , ResponseId).
-event(head_after_end, json(_)).
+match(Json, writeHead(ResponseId)) :- func_pre(Json , 'writeHead' , _ , _ , ResponseId).
 
-match(head_after_end,json(O), notId(ForbiddenId)) :- func_pre(json(O) , _ , _ , _ , ResponseId) , ResponseId \= ForbiddenId.
-event(head_after_end, json(_)).
+match(Json, notId(ForbiddenId)) :- func_pre(Json , _ , _ , _ , ResponseId) , ResponseId \= ForbiddenId.
 
-match(head_after_end,json(O), filterId(ResponseId)) :- func_pre(json(O) , _ , _ , _ , ResponseId).
-event(head_after_end, json(_)).
+match(Json, filterId(ResponseId)) :- func_pre(Json , _ , _ , _ , ResponseId).
 
 
 trace_expression('head_after_end', Main) :-
 Main = (filter >> ((createServer : Start))),
-Start = var( ResponseId, ((HeadThenEnd \/ OnlyEnd))),
-HeadThenEnd = (writeHead(ResponseId) :((((end(ResponseId) :epsilon)) | Another))),
-OnlyEnd = (end(ResponseId) :Another),
-Another = (((notId(ResponseId) >> Start)) /\ ((filterId(ResponseId) >> epsilon))),
-numbervars(Main, 0, _).
+Start = var( responseId, ((HeadThenEnd \/ OnlyEnd))),
+HeadThenEnd = (writeHead(var(responseId)) :((((end(var(responseId)) :eps)) | Another))),
+OnlyEnd = (end(var(responseId)) :Another),
+Another = (((notId(var(responseId)) >> Start)) /\ ((filterId(var(responseId)) >> eps))).
