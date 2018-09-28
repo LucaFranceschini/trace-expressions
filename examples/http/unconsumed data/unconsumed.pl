8cc82a22edeb35ed3f392bb4662a08b0f61044fe
davideancona <davide.ancona@unige.it>
2018-09-28 04:22:44 +0200

    benchmark

--- examples/http/unconsumed data/unconsumed.pl
+++ examples/http/unconsumed data/unconsumed.pl
@@ -1,23 +1,14 @@
-:- module(spec,[trace_expression/2, match/3]).
-
-:- discontiguous match/3.
-:- discontiguous event/2.
+:- module(spec,[trace_expression/2, match/2]).
 
 :- use_module(node(func_match)).
 
-match(unconsumed,json(O), filter) :- func_pre(json(O) , 'on' , _ , ['data'|_] , _).
-event(unconsumed, json(O)).
+match(Json, filter) :- func_pre(Json , 'on' , _ , ['data'|_] , _).
 
-match(unconsumed,json(O), filter) :- func_pre(json(O) , 'on' , _ , ['end'|_] , _).
-event(unconsumed, json(O)).
+match(Json, filter) :- func_pre(Json , 'on' , _ , ['end'|_] , _).
 
-match(unconsumed,json(O), onData(ResponseId)) :- func_pre(json(O) , 'on' , _ , ['data'|_] , ResponseId).
-event(unconsumed, json(O)).
+match(Json, onData(ResponseId)) :- func_pre(Json , 'on' , _ , ['data'|_] , ResponseId).
 
-match(unconsumed,json(O), onEnd(ResponseId)) :- func_pre(json(O) , 'on' , _ , ['end'|_] , ResponseId).
-event(unconsumed, json(O)).
-
+match(Json, onEnd(ResponseId)) :- func_pre(Json , 'on' , _ , ['end'|_] , ResponseId).
 
 trace_expression('unconsumed', filter >> Main) :-
-Main = var( ResponseId, (onData(ResponseId) :((((onEnd(ResponseId) :epsilon)) | Main)))),
-numbervars(Main, 0, _).
+Main = var( responseId, (onData(var(responseId)) :((((onEnd(var(responseId)) :eps)) | Main)))).
