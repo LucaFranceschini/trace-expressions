40ad2443d1fde49b3963d16c7336fb98b27a7db7
davideancona <davide.ancona@unige.it>
2018-10-23 17:15:17 +0200

    instrumentation

--- jalangi/callbacksInstrumentation.js
+++ jalangi/callbacksInstrumentation.js
@@ -19,65 +19,47 @@ for (const k in response) {
 		functions.push(obj)
 }
 
-function monitoredName(data) { // DA: Oct 1, 2018, optimization to monitor functions with names with initParams.names
-    return data.event.startsWith('cb') || !J$.initParams.names || J$.initParams.names.includes(data.functionName);
+function skip(data) { // optimization to monitor functions with names with initParams.names
+    return !data.functionObject._jalangi_callId && J$.initParams.names && !J$.initParams.names.includes(data.functionName);
 }
 
-function beforeFunction(data,ws) { // DA: Oct 12, 2018 added websocket  
-	/*if (!functions.includes(data.functionObject))
-		return data*/
-
-  //if(!filteredFunctionNames.includes(data.functionName)) return data
-  
-  /*if (data.functionName === 'emit') {
-  	console.log(data.arguments[0] + ' EMITTED BY SERVERRESPONSE AT')
-  	console.log(data.location)
-  }*/
-
-    // check for callbacks in the last argument
-
-	const args = data.arguments, argc = args.length;
-	if (argc > 0 && typeof args[argc-1] === 'function' && isInSupportedModule(data)) {
-		const cb = args[argc-1];
-		
-		// wrap the callback and store the call ID
-		function wrapper() {
-			cb._jalangi_callId = data.callId;
-			return cb.apply(data.target, arguments);
-		}
-		
-		args[argc-1] = wrapper;
-	}
-
-	// check if this function is a callback
-	if (data.functionObject._jalangi_callId) {
-		data.callId = data.functionObject._jalangi_callId;
-		data.event = 'cb_pre';
-	}
-	else
-		data.event = 'func_pre';
-
-    if(monitoredName(data)) // DA: Oct 1, 2018, optimization to monitor names with initParams.names
-	ws.newEvent(data); // DA: Oct 12, 2018, uses websocket 
-	
+function beforeFunction(data,ws) { 
+    if(skip(data)) // optimization to monitor names with initParams.names
 	return data;
+    const args = data.arguments, argc = args.length;
+    if (argc > 0 && typeof args[argc-1] === 'function' && isInSupportedModule(data)) {
+	const cb = args[argc-1];
+	// wrap the callback and store the call ID
+	function wrapper() {
+	    cb._jalangi_callId = data.callId;
+	    return cb.apply(data.target, arguments);
+	}
+	args[argc-1] = wrapper;
+    }
+    // check if this function is a callback
+    if (data.functionObject._jalangi_callId) {
+	data.callId = data.functionObject._jalangi_callId;
+	data.event = 'cb_pre';
+    }
+    else
+	data.event = 'func_pre';
+    ws.newEvent(data);
+    return data;
 }
 
 function isInSupportedModule(data) {
 	return supportedModules.some((m, i, a) => data.functionName.startsWith(m));
 }
 
-function afterFunction(data,ws) { // DA: Oct 12, 2018 added websocket  
-    //	check if it is a callback
-    if(J$.initParams.func_post){
-    	if (data.functionObject._jalangi_callId) {
-    	    data.callId = data.functionObject._jalangi_callId;
-    	    data.event = 'cb_post';
-    	}
-    	else
-    	    data.event = 'func_post';	
-	if(monitoredName(data)) // DA: Oct 1, 2018, optimization to monitor names with initParams.names
-	    ws.newEvent(data); // DA: Oct 12, 2018, uses websocket 
-    }	
+function afterFunction(data,ws) { 
+    if(!J$.initParams.func_post || skip(data))
 	return data;
+    if (data.functionObject._jalangi_callId) {
+    	data.callId = data.functionObject._jalangi_callId;
+    	data.event = 'cb_post';
+    }
+    else
+    	data.event = 'func_post';	
+    ws.newEvent(data); 
+    return data;
 }
