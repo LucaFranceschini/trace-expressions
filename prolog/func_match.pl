2fc9033e9913fc2d89dec5d109954ebe5d32f773
davideancona <davide.ancona@unige.it>
2018-10-30 23:44:08 +0100

    monitor

--- prolog/func_match.pl
+++ prolog/func_match.pl
@@ -1,7 +1,7 @@
 :- module(func_match, [
-	func_pre/5, func_pre_name/2, func_pre_names/2,
+	func_pre/5, func_pre/6, func_pre_name/2, func_pre_names/2,
 	func_post/5, func_post_name/2, func_post_names/2,
-	cb_pre/5, cb_pre/1, cb_post/5
+	cb_pre/5, cb_pre/6, cb_pre/1, cb_post/5
 ]).
     
 % low-level JSON parsing
@@ -13,7 +13,10 @@
 
 parse_json_pre(Json, Json.get(event), Json.get(name), Json.get(id), Json.get(args), Json.get(targetId)).
 
-%% equivalent clause with debugging showing the bug %%
+%% traces also argument ids 
+parse_json_pre(Json, Json.get(event), Json.get(name), Json.get(id), Json.get(args), Json.get(argIds), Json.get(targetId)).
+
+%% equivalent clause with debugging showing the bug. Resolved, see the Prolog server  %%
 %% parse_json_pre(Json,E, N,I,A,T) :- writeln('parse_json_pre'),JE=Json.get(event),writeln(E),writeln(JE),JE=E,writeln('done'),N=Json.get(name), I=Json.get(id), A=Json.get(args), T=Json.get(targetId).
 
 parse_json_post(Json, Json.get(event), Json.get(name), Json.get(args), Json.get(res), Json.get(resultId)).
@@ -24,12 +27,18 @@ parse_json_post(Json, Json.get(event), Json.get(name), Json.get(args), Json.get(
 func_pre(Json, Name, Id, Args, TargetId) :-
 	parse_json_pre(Json, func_pre, Name, Id, Args, TargetId).
 
+func_pre(Json, Name, Id, Args, ArgIds, TargetId) :-  %% traces also argument ids 
+	parse_json_pre(Json, func_pre, Name, Id, Args, ArgIds, TargetId).
+
 func_post(Json, Name, Args, Res, ResultId) :-
 	parse_json_post(Json, func_post, Name, Args, Res, ResultId).
 
 cb_pre(Json, Name, Id, Args, TargetId) :-
 	parse_json_pre(Json, cb_pre, Name, Id, Args, TargetId).
 
+cb_pre(Json, Name, Id, Args, ArgIds, TargetId) :- %% traces also argument ids 
+	parse_json_pre(Json, cb_pre, Name, Id, Args, ArgIds, TargetId).
+    
 cb_post(Json, Name, Args,  Res, ResultId) :-
 	parse_json_post(Json, cb_post, Name, Args, Res, ResultId).
 
