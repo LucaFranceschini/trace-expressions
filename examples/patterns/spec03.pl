42bbafb32db4a244d3f1368108a22aefe8c2a2a9
davideancona <davide.ancona@unige.it>
2018-11-11 23:06:29 +0100

    useful specification patterns assumed resource ids are just integers (and not objects) and are passed as arguments

--- examples/patterns/spec03.pl
+++ examples/patterns/spec03.pl
@@ -21,13 +21,13 @@ match(Json, acquire_release) :- func_post_name(Json , 'acquire').
      
 match(Json, acquire_release) :- func_pre_name(Json , 'release').
 
-match(Json, acquire(ResId)) :- func_post(Json, 'acquire', _Args, _Res, ResId).
+match(Json, acquire(Res)) :- func_post(Json, 'acquire', _Args, Res).
 
 match(Json, acquire) :- match(Json, acquire(_)).
 
-match(Json, use(ResourceId)) :- func_pre(Json, 'use', _Id, _Args, _ArgIds, ResourceId).
+match(Json, use(ResourceId)) :- func_pre(Json, 'use', _Id, [ResourceId|_], _TargetId).
 
-match(Json, release(ResourceId)) :- func_pre(Json, 'release', _Id, _Args, _ArgIds, ResourceId).
+match(Json, release(ResourceId)) :- func_pre(Json, 'release', _Id, [ResourceId|_], _TargetId).
 
 match(Json, release) :- match(Json, release(_)).
 
@@ -42,4 +42,4 @@ trace_expression('test', filter >> (MainFlow /\ Exclusion)) :-
     AnyRelease = eps\/(release:AnyRelease).  %%% this is yet another kind of filter
 
 %% a simple test    
-%% trace_expression(_,T0),next(T0,_{event:func_post,name:acquire,args:[],res:0,resultId:41},T1),next(T1,_{event:func_post,name:acquire,args:[],res:0,resultId:42},T2),next(T2,_{event:func_pre,name:release,args:[],argIds:[],id:0,targetId:41},T3),next(T3,_{event:func_post,name:acquire,args:[],res:0,resultId:41},T4),next(T4,_{event:func_pre,name:release,args:[],argIds:[],id:0,targetId:41},T5),next(T5,_{event:func_pre,name:release,args:[],argIds:[],id:0,targetId:42},T6),next(T6,_{event:func_post,name:acquire,args:[],res:0,resultId:41},T7),next(T7,_{event:func_pre,name:use,args:[],argIds:[],id:0,targetId:41},T8),next(T8,_{event:func_post,name:acquire,args:[],res:0,resultId:42},T9),next(T9,_{event:func_pre,name:release,args:[],argIds:[],id:0,targetId:42},T10),next(T10,_{event:func_pre,name:release,args:[],argIds:[],id:0,targetId:41},T11).
+%% trace_expression(_,T0),next(T0,_{event:func_post,name:acquire,args:[],res:41},T1),next(T1,_{event:func_post,name:acquire,args:[],res:42},T2),next(T2,_{event:func_pre,name:release,args:[41],id:0,targetId:0},T3),next(T3,_{event:func_post,name:acquire,args:[],res:41},T4),next(T4,_{event:func_pre,name:release,args:[41],id:0,targetId:0},T5),next(T5,_{event:func_pre,name:release,args:[42],id:0,targetId:0},T6),next(T6,_{event:func_post,name:acquire,args:[],res:41},T7),next(T7,_{event:func_pre,name:use,args:[41],id:0,targetId:0},T8),next(T8,_{event:func_post,name:acquire,args:[],res:42},T9),next(T9,_{event:func_pre,name:release,args:[42],id:0,targetId:0},T10),next(T10,_{event:func_pre,name:release,args:[41],id:0,targetId:0},T11).
