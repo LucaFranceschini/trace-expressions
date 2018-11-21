55c1a0ac24b541a1f91cc2722c87dd07ea4c54f5
davideancona <davide.ancona@unige.it>
2018-11-21 06:29:36 +0100

    benchmark

--- examples/patterns/spec06.pl
+++ examples/patterns/spec06.pl
@@ -1,6 +1,6 @@
 %%% Specification for queues  %%%
 
-:- module(spec,[trace_expression/2, match/2, gtzero/1, inc/2]).
+:- module(spec,[trace_expression/2, match/2]).
 
 :- use_module(node(func_match)).
 
@@ -8,34 +8,20 @@ match(Json, filter) :- func_pre_name(Json, 'enq').
 
 match(Json, filter) :- func_post_name(Json , 'deq').
 
-match(Json, enq(Elem)) :- func_pre(Json, 'enq', _Id, [Elem], _TargetId).
+match(Json, enq(Elem,Size,NewSize)) :- func_pre(Json, 'enq', _Id, [Elem], _TargetId),integer(Size),NewSize is Size+1.
 
-match(Json, deq(Elem)) :- func_post(Json, 'deq', _Args, Elem).
+match(Json, deq(Elem,Size,NewSize)) :- func_post(Json, 'deq', _Args, Elem),integer(Size),Size>0,NewSize is Size-1.
 
-match(Json, enq) :- match(Json, enq(_Elem)).
+match(Json, enq(Size,NewSize)) :- match(Json, enq(_Elem,Size,NewSize)).
 
-match(Json, deq) :- match(Json, deq(_Elem)).
+match(Json, deq(Size,NewSize)) :- match(Json, deq(_Elem,Size,NewSize)).
 
-gtzero(N) :- N > 0.
-
-inc(M,N) :- integer(M),N is M+1,!;integer(N),M is N-1.
-    
-%% this spec enforces that acquire(resId) cannot follow another acquire(resId) without a release(resId) in between
 trace_expression('test', filter >>  app(Queue,0)) :- 
     Queue = gen(size,
 		eps\/
-		(enq:var(newSize,
-			 guarded(inc(var(size),var(newSize)),1,0)/\ %% this should be a derived operator  
-			 app(Queue,var(newSize))
-			))\/
-		guarded(
-			gtzero(var(size)),
-			deq:var(newSize,
-				guarded(inc(var(newSize),var(size)),1,0)/\ %% this should be a derived operator  
-				app(Queue,var(newSize))
-			       ),
-			0)
+		var(newSize,enq(var(size),var(newSize)):app(Queue,var(newSize)))\/
+		var(newSize,deq(var(size),var(newSize)):app(Queue,var(newSize)))
 	       ).
 
 %% a simple test
-%% trace_expression(_,T0),next(T0,_{event:func_pre,name:enq,args:[1],id:0,targetId:0},T1), next(T1,_{event:func_pre,name:enq,args:[2],id:0,targetId:0},T2),next(T2,_{event:func_post,name:deq,args:[],res:1},T3),next(T3,_{event:func_pre,name:enq,args:[3],id:0,targetId:0},T4).
+%% trace_expression(_,T0),next(T0,_{event:func_pre,name:enq,args:[1],id:0,targetId:0},T1), next(T1,_{event:func_pre,name:enq,args:[2],id:0,targetId:0},T2),next(T2,_{event:func_post,name:deq,args:[],res:1},T3),next(T3,_{event:func_pre,name:enq,args:[3],id:0,targetId:0},T4),next(T4,_{event:func_post,name:deq,args:[],res:1},T5),next(T5,_{event:func_post,name:deq,args:[],res:1},T6),next(T6,_{event:func_pre,name:enq,args:[1],id:0,targetId:0},T7).
