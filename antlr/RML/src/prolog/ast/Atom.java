3d2d3b907c2e0a5b51202da720799e1346a82b28
Luca Franceschini <luca.franceschini92@gmail.com>
2018-11-30 18:14:55 +0100

    error

--- antlr/RML/src/prolog/ast/Atom.java
+++ antlr/RML/src/prolog/ast/Atom.java
@@ -4,8 +4,8 @@ import util.AbstractFunctionTerm;
 
 import java.util.List;
 
-public final class Atom extends AbstractFunctionTerm<Symbol, FuncTerm> {
-    public Atom(Symbol id, List<FuncTerm> args) {
+public final class Atom extends AbstractFunctionTerm<Symbol, Term> {
+    public Atom(Symbol id, List<Term> args) {
         super(id, args);
     }
 
