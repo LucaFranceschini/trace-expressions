f900a42ad22871e9eabc4f89afc95da37b8874f0
Luca Franceschini <luca.franceschini92@gmail.com>
2018-11-30 18:14:04 +0100

    checks on identifier characters

--- antlr/RML/src/prolog/ast/Symbol.java
+++ antlr/RML/src/prolog/ast/Symbol.java
@@ -1,13 +1,23 @@
 package prolog.ast;
 
-import util.AbstractIdentifier;
+import java.nio.charset.StandardCharsets;
 
 // predicate or function symbol
-public final class Symbol extends AbstractIdentifier {
-    public Symbol(String name) {
-        super(name);
+public final class Symbol {
+    private final String string;
 
-        if (Character.isLowerCase(name.charAt(0)))
-            throw new IllegalArgumentException("predicate and function symbols should start with lowercase letters");
+    public Symbol(String string) {
+        if (!StandardCharsets.US_ASCII.newEncoder().canEncode(string))
+            throw new IllegalArgumentException("ASCII string expected");
+
+        if (string.chars().anyMatch(Character::isISOControl))
+            throw new IllegalArgumentException("Printable characters expected");
+
+        if (string.chars().anyMatch(Character::isWhitespace))
+            throw new IllegalArgumentException("Whitespace not allowed");
+
+        this.string = string;
     }
+
+    public String string() { return string; }
 }
